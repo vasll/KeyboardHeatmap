d8181d8744f365b96a873bfad90aa8b779985147
vasll <valentinosoll17@gmail.com>
2023-03-15 17:24:46 +0100

    Added more keyboard layouts

--- src/stores/KeyboardLayoutStore.js
+++ src/stores/KeyboardLayoutStore.js
@@ -36,4 +36,39 @@ export const KeyboardLayoutStore = writable({
             ["z", "x", "c", "d", "v", "k", "h"]
         ]
     },
+    "qwertz": {
+        keyboardRows: [
+            ["q", "w", "e", "r", "t", "z", "u", "i", "o", "p"],
+            ["a", "s", "d", "f", "g", "h", "j", "k", "l"],
+            ["y", "x", "c", "v", "b", "n", "m"]
+        ]
+    },
+    "dvorak": {
+        keyboardRows: [
+            ["p", "y", "f", "g", "c", "r", "l"],
+            ["a", "o", "e", "u", "i", "d", "h", "t", "n", "s"],
+            ["q", "j", "k", "x", "b", "m", "w", "v", "z"]
+        ]
+    },
+    "qzerty": {
+        keyboardRows: [
+            ["q", "z", "e", "r", "t", "y", "u", "i", "o", "p"],
+            ["a", "s", "d", "f", "g", "h", "j", "k", "l"],
+            ["w", "x", "c", "v", "b", "n", "m"]
+        ]
+    },
+    "workman": {
+        keyboardRows: [
+            ["q", "d", "r", "w", "b", "j", "f", "u", "p"],
+            ["a", "s", "h", "t", "g", "y", "n", "e", "o", "i"],
+            ["z", "x", "m", "c", "v", "l", "k"]
+        ]
+    },
+    "norman": {
+        keyboardRows: [
+            ["q", "w", "f", "p", "g", "j", "l", "u", "y"],
+            ["a", "r", "s", "t", "d", "h", "n", "e", "i", "o"],
+            ["z", "x", "c", "v", "b", "k", "m"]
+        ]
+    }
 })
